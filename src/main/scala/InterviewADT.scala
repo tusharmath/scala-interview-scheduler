fae563f6f7c17e51c7585813ea38e48c3f973e98
Tushar Mathur <tusharmath@gmail.com>
2020-03-07 12:01:03 +0530

    refactor: create a general `Resource` type.

--- src/main/scala/InterviewADT.scala
+++ src/main/scala/InterviewADT.scala
@@ -4,6 +4,8 @@ import scala.concurrent.duration._
 import scala.language.postfixOps
 
 object InterviewADT {
+  trait Resource
+
   // TODO: add skill priority
   case class Skill(name: String)
 
@@ -13,12 +15,13 @@ object InterviewADT {
       name: String,
       skills: Set[Skill] = Set.empty,
       company: Option[String] = None
-  )
+  ) extends Resource
 
   // TODO: add interviewer level
   case class Interviewer(name: String, skills: Set[Skill] = Set.empty)
+      extends Resource
 
-  case class Room(name: String)
+  case class Room(name: String) extends Resource
 
   // TODO: add banned interviewer-candidate combinations
   case class EventAvailability(
@@ -28,11 +31,6 @@ object InterviewADT {
       candidateSkillMap: Map[Candidate, Set[Skill]] = Map.empty,
       interviewDuration: Duration = 1 hour,
       interviewStartTime: LocalDateTime = LocalDateTime.now()
-  )
-
-  case class Combination(
-      interview: Interview,
-      availability: EventAvailability
   )
 
   case class Interview(
