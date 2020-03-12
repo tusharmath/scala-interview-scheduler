1b00cb09310d97532ab49284618439c3991609ab
Tushar Mathur <tusharmath@gmail.com>
2020-03-12 17:10:06 +0530

    feat: add a experience parameter

--- src/main/scala/InterviewADT.scala
+++ src/main/scala/InterviewADT.scala
@@ -5,6 +5,13 @@ import scala.concurrent.duration._
 object InterviewADT {
   sealed trait Resource
 
+  trait Experience
+  object Experience {
+    object High   extends Experience
+    object Medium extends Experience
+    object Low    extends Experience
+  }
+
   // TODO: add skill priority
   case class Skill(name: String)
 
@@ -17,31 +24,31 @@ object InterviewADT {
   ) extends Resource
 
   // TODO: add interviewer level
-  case class Interviewer(name: String, skills: Set[Skill] = Set.empty)
-      extends Resource
+  case class Interviewer(
+      // Interviewer's name
+      name: String,
+      // Interviewer's Skills
+      skills: Set[Skill] = Set.empty,
+      // How preferred is this Interviewer
+      // Typically we want to save certain interviewers for later
+      preference: Experience = Experience.Medium
+  ) extends Resource
 
   case class Room(name: String) extends Resource
 
-  // TODO: add banned interviewer-candidate combinations
   case class EventAvailability(
       // Interviewers for the interview
       interviewers: Set[Interviewer] = Set.empty,
-
       // Candidates for the interview
       candidates: Set[Candidate] = Set.empty,
-
       // Rooms available for the interview
       rooms: Set[Room] = Set.empty,
-
       // Skills that have been interviewed for
       candidateSkillMap: Map[Candidate, Set[Skill]] = Map.empty,
-
       // Time period for one interview
       interviewDuration: Duration = 1 hour,
-
       // Starting time for the interview
       interviewStartTime: LocalDateTime = LocalDateTime.now(),
-
       // Referrals
       referrals: Set[(Candidate, Interviewer)] = Set.empty
   )
