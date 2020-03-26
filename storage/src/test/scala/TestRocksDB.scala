1aa637fdb21482b4174f76212adc43d690e4b9eb
Tushar Mathur <tusharmath@gmail.com>
2020-03-26 11:25:58 +0530

    test(storage): move test db to a separate project

---
+++ storage/src/test/scala/TestRocksDB.scala
@@ -0,0 +1,24 @@
+import java.nio.file.{Files, Path}
+
+import zio._
+import zio.rocksdb.RocksDB
+import zio.test.TestFailure
+
+import scala.reflect.io
+
+object TestRocksDB {
+  def live: ZLayer[Any, TestFailure[Nothing], Has[RocksDB.Service]] =
+    (for {
+      dir     <- managedDirectory
+      service <- rocksdb.Live.open(dir.toAbsolutePath.toString)
+    } yield service).toLayer.mapError(TestFailure.die)
+
+  private def managedDirectory =
+    Task(Files.createTempDirectory("storage"))
+      .toManaged(deletePath)
+
+  private def deletePath(path: Path): UIO[Boolean] =
+    IO {
+      new io.Directory(path.toFile).deleteRecursively()
+    } <> UIO(false)
+}
